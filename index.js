98975052c1e1ff2ff688d44315440cbe6aa5621f
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2024-09-12 19:57:36 +0700

    Init UI

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/test", (req, res) => {
+  res.send("API work wells!");
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
