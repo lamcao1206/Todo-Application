2f6514e963378e8523aa338727ff5c4330def082
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2024-09-12 20:08:56 +0700

    adding icon button

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-app.set("views", "views");
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 3000;
 
